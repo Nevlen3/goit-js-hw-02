1094599442fdbe79111e6cb089ed13576641d838
Nevlen3 <nevlen3@gmail.com>
2025-04-12 11:36:35 +0200

    1
    1

--- js/task-3.js
+++ js/task-3.js
@@ -5,7 +5,3 @@ function getElementWidth(content, padding, border) {
 
   return contentValue + paddingValue + borderValue;
 }
-
-console . log (getElementWidth( "50px" , "8px" , "4px" )); // 74 
-console . log (getElementWidth( "60px" , "12px" , "8.5px" )); // 101 
-console . log (getElementWidth( "200px" , "0px" , "0px" )); // 200
