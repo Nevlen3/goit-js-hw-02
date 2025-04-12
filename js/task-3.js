580dc46406f348d593bfbf302581ec6d5a497492
Nevlen3 <nevlen3@gmail.com>
2025-04-12 11:40:50 +0200

    finish
    js

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,13 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const paddingValue = parseFloat(padding) * 2;
-  const borderValue = parseFloat(border) * 2;
-
-  return contentValue + paddingValue + borderValue;
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
+
+console . log (checkForSpam( "Latest technology news" )); // false 
+console . log (checkForSpam( "JavaScript weekly newsletter" )); // false 
+console . log (checkForSpam( "Get best sale offers now!" )); // true 
+console . log (checkForSpam( "Amazing SalE, only tonight!" )); // true 
+console . log (checkForSpam( "Trust me, this is not a spam message" )); // true 
+console . log (checkForSpam( "Get rid of sPaM emails. Our book in on sale!" )); // true 
+console . log (checkForSpam( "[SPAM] How to earn fast money?" )); // true
+
